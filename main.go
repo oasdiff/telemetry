6202644bcd71fafa59aefc9e260ab1dff1e4c0bc
effoeffi <effoeffi@gmail.com>
2023-08-26 18:26:38 +0300

    test cloud task

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	taskspb "cloud.google.com/go/cloudtasks/apiv2/cloudtaskspb"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/oasdiff/go-common/env"
 	"github.com/oasdiff/telemetry/model"
 	"golang.org/x/exp/slog"
 )
@@ -20,7 +26,6 @@ func setupRouter() *gin.Engine {
 
 	router := gin.Default()
 	router.POST(fmt.Sprintf("/%s", model.KeyEvents), func(ctx *gin.Context) {
-
 		var events map[string][]*model.Telemetry
 		if err := ctx.BindJSON(&events); err != nil {
 			return
@@ -46,6 +51,41 @@ func setupRouter() *gin.Engine {
 
 		ctx.Writer.WriteHeader(http.StatusCreated)
 	})
+	router.GET("/test", func(ctx *gin.Context) {
+		client, err := cloudtasks.NewClient(context.Background())
+		if err != nil {
+			slog.Error("failed to create cloud task client", "error", err)
+			ctx.Writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer client.Close()
+
+		request := &taskspb.CreateTaskRequest{
+			Parent: fmt.Sprintf("projects/%s/locations/%s/queues/%s", env.GetGCPProject(), env.GetGCPLocation(), env.GetGCPQueue()),
+			Task: &taskspb.Task{
+				MessageType: &taskspb.Task_HttpRequest{
+					HttpRequest: &taskspb.HttpRequest{
+						HttpMethod: taskspb.HttpMethod_POST,
+						Url:        env.GetTaskSubscriberUrl(),
+					},
+				},
+			},
+		}
+		request.Task.GetHttpRequest().Body = streamToByte(ctx.Request.Body)
+
+		_, err = client.CreateTask(ctx, request)
+		if err != nil {
+			ctx.Writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
 
 	return router
+}
+
+func streamToByte(stream io.Reader) []byte {
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.Bytes()
 }
