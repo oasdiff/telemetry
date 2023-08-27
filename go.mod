b55c3044be990d37a5e62470da25c5b53175484a
effoeffi <effoeffi@gmail.com>
2023-08-27 12:29:27 +0300

    size limit

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	cloud.google.com/go/cloudtasks v1.12.1
 	github.com/denisbrodbeck/machineid v1.0.1
 	github.com/gin-gonic/gin v1.9.1
-	github.com/google/uuid v1.3.1
 	github.com/oasdiff/go-common v0.2.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
 	taskspb "cloud.google.com/go/cloudtasks/apiv2/cloudtaskspb"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/oasdiff/go-common/env"
 	"github.com/oasdiff/telemetry/model"
 	"golang.org/x/exp/slog"
@@ -23,34 +21,9 @@ func main() {
 
 func setupRouter() *gin.Engine {
 
+	builder := newTaskBuilder()
 	router := gin.Default()
 	router.POST(fmt.Sprintf("/%s", model.KeyEvents), func(ctx *gin.Context) {
-		var events map[string][]*model.Telemetry
-		if err := ctx.BindJSON(&events); err != nil {
-			return
-		}
-
-		if len(events) > 1 {
-			slog.Info("user sent more than 1 telemetry events", "count", len(events))
-			ctx.Writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		for _, currEvent := range events[model.KeyEvents] {
-			currEvent.Id = uuid.NewString()
-		}
-
-		payload, err := json.Marshal(events)
-		if err != nil {
-			slog.Error("failed to 'Marshal'", "events", len(events), "error", err)
-			ctx.Writer.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		slog.Info(string(payload))
-
-		ctx.Writer.WriteHeader(http.StatusCreated)
-	})
-	router.GET("/test", func(ctx *gin.Context) {
 		client, err := cloudtasks.NewClient(context.Background())
 		if err != nil {
 			slog.Error("failed to create cloud task client", "error", err)
@@ -59,34 +32,60 @@ func setupRouter() *gin.Engine {
 		}
 		defer client.Close()
 
-		request := &taskspb.CreateTaskRequest{
-			Parent: fmt.Sprintf("projects/%s/locations/%s/queues/%s", env.GetGCPProject(), env.GetGCPLocation(), env.GetGCPQueue()),
-			Task: &taskspb.Task{
-				MessageType: &taskspb.Task_HttpRequest{
-					HttpRequest: &taskspb.HttpRequest{
-						HttpMethod: taskspb.HttpMethod_POST,
-						Url:        env.GetTaskSubscriberUrl(),
-					},
-				},
-			},
-		}
-
 		buf := new(bytes.Buffer)
 		if _, err := buf.ReadFrom(ctx.Request.Body); err != nil {
 			slog.Error("failed to read request body", "error", err)
 			ctx.Writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		request.Task.GetHttpRequest().Body = buf.Bytes()
-		ctx.Request.Body.Close()
+		defer ctx.Request.Body.Close()
 
-		_, err = client.CreateTask(ctx, request)
+		body := buf.Bytes()
+		size := len(body)
+		if size > 102400 { // 100KB
+			slog.Info("client sent payload > 100KB", "size", size)
+			ctx.Writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		_, err = client.CreateTask(ctx, builder.CreateRequest(body))
 		if err != nil {
 			slog.Error("failed to create cloud task", "error", err)
 			ctx.Writer.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		ctx.Writer.WriteHeader(http.StatusCreated)
 	})
 
 	return router
+}
+
+type taskBuilder struct {
+	requestParent string
+	subscriberUrl string
+}
+
+func newTaskBuilder() *taskBuilder {
+
+	return &taskBuilder{
+		requestParent: fmt.Sprintf("projects/%s/locations/%s/queues/%s", env.GetGCPProject(), env.GetGCPLocation(), env.GetGCPQueue()),
+		subscriberUrl: env.GetTaskSubscriberUrl(),
+	}
+}
+
+func (b *taskBuilder) CreateRequest(body []byte) *taskspb.CreateTaskRequest {
+
+	return &taskspb.CreateTaskRequest{
+		Parent: b.requestParent,
+		Task: &taskspb.Task{
+			MessageType: &taskspb.Task_HttpRequest{
+				HttpRequest: &taskspb.HttpRequest{
+					HttpMethod: taskspb.HttpMethod_POST,
+					Url:        b.subscriberUrl,
+					Body:       body,
+				},
+			},
+		},
+	}
 }
